ebfc3d5e96b5fc58b50c35ef2b74a3f6d67c04f8
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2020-08-10 04:05:05 +0900

    Add /api/v1

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,10 @@ app.get('/', (_, res) => {
   res.send('Hello World!');
 });
 
+app.get('/api/v1', (_, res) => {
+  res.json({message: 'Hello, World!'});
+});
+
 app.listen(port, () => {
   console.error(`Example app listening at http://localhost:${port}`);
 });
