c683b2f5a9cb0ca7dcef53f7c06a872682056d2a
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2020-08-16 17:13:28 +0900

    Setup error handler

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,13 @@
 import { app } from './app';
+import {
+  productionErrorHandler,
+  developmentErrorHandler,
+} from './middlewares/error_handlers';
 
 const port = Number(process.env.PORT) || 3000;
+const isProduction = process.env.NODE_ENV === 'production';
+
+app.use(isProduction ? productionErrorHandler : developmentErrorHandler);
 
 app.listen(port, () => {
   console.error(`Example app listening at http://localhost:${port}`);
