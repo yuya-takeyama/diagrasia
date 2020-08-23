e47531c6c7ecbc15735a282dd800ed26018a1692
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2020-08-23 15:14:06 +0900

    Add images bucket

---
+++ terraform/gcp/images-bucket.tf
@@ -0,0 +1,45 @@
+resource "google_storage_bucket" "images" {
+  name          = "diagrasia-images"
+  location      = "ASIA-NORTHEAST1"
+  force_destroy = true
+
+  bucket_policy_only = true
+}
+
+resource "google_storage_bucket_iam_policy" "images" {
+  bucket = "b/diagrasia-images"
+
+  policy_data = <<POLICY
+{
+  "bindings": [
+    {
+      "members": [
+        "projectEditor:diagrasia",
+        "projectOwner:diagrasia"
+      ],
+      "role": "roles/storage.legacyBucketOwner"
+    },
+    {
+      "members": [
+        "projectViewer:diagrasia"
+      ],
+      "role": "roles/storage.legacyBucketReader"
+    },
+    {
+      "members": [
+        "projectEditor:diagrasia",
+        "projectOwner:diagrasia"
+      ],
+      "role": "roles/storage.legacyObjectOwner"
+    },
+    {
+      "members": [
+        "allUsers",
+        "projectViewer:diagrasia"
+      ],
+      "role": "roles/storage.legacyObjectReader"
+    }
+  ]
+}
+POLICY
+}
